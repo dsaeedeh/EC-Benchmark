09a4afec278682f1d6227766efee235bcdfede04
davoudisaeedeh <saeedeh.davoudi@ucdenver.edu>
2023-07-08 11:56:12 -0600

    modified:   download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,22 +1,6 @@
-mkdir data
+#!/bin/bash
+#SBATCH --job-name=extract_coordinates
+#SBATCH --partition=math-alderaan
+#SBATCH --time=7-00:00:00  
 
-$1: 2022
-$2: 05
-$3: 2018
-$4: 02
-
-# Download testing Swissprot $1 $2
-curl https://ftp.uniprot.org/pub/databases/uniprot/previous_releases/release-$1_$2/knowledgebase/uniprot_sprot-only$1_$2.tar.gz -o uniprot_sprot-only$1_$2.tar.gz
-
-# Download pretraining and training $3 $4
-curl https://ftp.uniprot.org/pub/databases/uniprot/previous_releases/release-$3_$4/knowledgebase/knowledgebase$3_$4.tar.gz -o knowledgebase$3_$4.tar.gz
-
-# Swissprot - test
-tar -xf uniprot_sprot-only$1_$2.tar.gz uniprot_sprot.xml.gz
-gunzip uniprot_sprot.xml.gz
-mv uniprot_sprot.xml uniprot_sprot_$1_$2.xml 
-
-# pretraining and training
-tar -xf knowledgebase$3_$4.tar.gz 
-gunzip uniprot_trembl$3_$4.dat.gz
-mv uniprot_sprot.xml uniprot_sprot_$1_$2.xml
+python3 code/download_data.py 2023 01 2018 02 >& download_data.log
