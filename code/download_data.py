95d655154206df10670a931cdddfa5fd338355a7
davoudisaeedeh <saeedeh.davoudi@ucdenver.edu>
2023-07-08 15:28:09 -0600

    correct the errors of download_data.py

--- code/download_data.py
+++ code/download_data.py
@@ -1,5 +1,8 @@
 import argparse
-import subprocess
+import urllib.request
+import os
+import tarfile
+import shutil
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description='Download and extract UniProt files')
@@ -14,23 +17,33 @@ month1 = args.month1
 year2 = args.year2
 month2 = args.month2
 
-# Change directory to data
-subprocess.run(["cd", "data"])
+# Set output directory
+output_dir = "data"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
 
 # Download testing Swissprot
 testing_url = f"https://ftp.uniprot.org/pub/databases/uniprot/previous_releases/release-{year1}_{month1}/knowledgebase/uniprot_sprot-only{year1}_{month1}.tar.gz"
-subprocess.run(["curl", testing_url, "-o", f"uniprot_sprot-only{year1}_{month1}.tar.gz"])
+urllib.request.urlretrieve(testing_url, output_dir + f"/uniprot_sprot-only{year1}_{month1}.tar.gz")
 
 # Download pretraining and training
 pretraining_url = f"https://ftp.uniprot.org/pub/databases/uniprot/previous_releases/release-{year2}_{month2}/knowledgebase/knowledgebase{year2}_{month2}.tar.gz"
-subprocess.run(["curl", pretraining_url, "-o", f"knowledgebase{year2}_{month2}.tar.gz"])
+urllib.request.urlretrieve(pretraining_url, output_dir + f"/knowledgebase{year2}_{month2}.tar.gz"])
 
-# Extract testing Swissprot
-subprocess.run(["tar", "-zxvf", f"uniprot_sprot-only{year1}_{month1}.tar.gz"])
-subprocess.run(["mv", "uniprot_sprot.dat", f"uniprot_sprot{year1}_{month1}.data"])
-subprocess.run(["rm", "-f", f"uniprot_sprot.fasta.gz", f"uniprot_sprot_varsplic.fasta.gz", f"uniprot_sprot.xml.gz"])
+# Extract uniprot_sprot
+tar_file = output_dir + f"/uniprot_sprot-only{year1}_{month1}.tar.gz"
+with tarfile.open(tar_file, "r:gz") as tar:
+    tar.extract("uniprot_sprot.dat.gz")
+os.rename("uniprot_sprot.dat.gz", f"uniprot_sprot{year1}_{month1}.data.gz")
+shutil.move(f"uniprot_sprot{year1}_{month1}.data.gz", output_dir + f"/uniprot_sprot{year1}_{month1}.data.gz")
 
-# Extract pretraining and training
-subprocess.run(["tar", "-zxvf", f"knowledgebase{year2}_{month2}.tar.gz"])
-subprocess.run(["mv", "uniprot_sprot.dat.gz", f"uniprot_sprot{year2}_{month2}.data.gz"])
-subprocess.run(["mv", "uniprot_trembl.dat.gz", f"uniprot_trembl{year2}_{month2}.data.gz"])
+# Extract knowledgebase
+tar_file = output_dir + f"/knowledgebase{year2}_{month2}.tar.gz"
+with tarfile.open(tar_file, "r:gz") as tar:
+    tar.extract("uniprot_sprot.dat.gz")
+    tar.extract("uniprot_trembl.dat.gz")
+os.rename("uniprot_sprot.dat.gz", f"uniprot_sprot{year2}_{month2}.data.gz")
+shutil.move(f"uniprot_sprot{year2}_{month2}.data.gz", output_dir + f"/uniprot_sprot{year2}_{month2}.data.gz")
+os.rename("uniprot_trembl.dat.gz", f"uniprot_trembl{year2}_{month2}.data.gz")
+shutil.move(f"uniprot_trembl{year2}_{month2}.data.gz", output_dir + f"/uniprot_trembl{year2}_{month2}.data.gz")
