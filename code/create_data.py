79909979e5278c57725ece44d45f864b0110d9c7
davoudisaeedeh <saeedeh.davoudi@ucdenver.edu>
2023-07-17 21:54:53 -0600

    renamed:    code/create_finetuning_data.py -> code/create_data.py

--- code/create_data.py
+++ code/create_data.py
@@ -59,27 +59,42 @@ def create_data_for_finetuning_task(train_ec_path, test_ec_path, train_3d_path, 
 
 # Create pretraining data
 '''
-1. Find similar sequences in pretrained data based on the clustering result and remove their EC numbers
+1. Find similar sequences in pretrained data based on the clustering result and remove their EC numbers from the data
 '''
+def create_data_for_pretraining_task(pretrain_ec_path, test_ec_path, clustering_path):
+    pretrain = pd.read_feather(pretrain_ec_path)
+    test = pd.read_csv(test_ec_path)
+
+    clusters = pd.read_csv(clustering_path, sep='\t', header=None)
+    ids_to_remove = []
+    test_ids = test['id'].tolist()
+
+    for i in range(clusters.shape[0]):
+        rep = clusters.iloc[i, 0]
+        cluster = clusters.iloc[i, 1]
+        if rep in test_ids or cluster in test_ids:
+            ids_to_remove.append(rep)
+            ids_to_remove.append(cluster)
+
+    ids_to_remove = list(set(ids_to_remove))
+    for i in range(pretrain.shape[0]):
+        if pretrain.iloc[i, 0] in ids_to_remove:
+            pretrain['ec_number'][i] = '-'
+
+    pretrain.to_csv('data/cluster-30/pretrain_ec.csv', index=False)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Fine-tuning data creation')
+    parser.add_argument('--pretrain_ec_path', type=str, default='data/pretrain_ec.feather', help='Path to pretrain ec data')
     parser.add_argument('--train_ec_path', type=str, default='data/train_ec.feather', help='Path to train ec data')
     parser.add_argument('--test_ec_path', type=str, default='data/test_ec.feather', help='Path to test ec data')
     parser.add_argument('--train_3d_path', type=str, default='data/train_having_3d.fasta', help='Path to train 3d data')
     parser.add_argument('--test_3d_path', type=str, default='data/test_having_3d.fasta', help='Path to test 3d data')
     parser.add_argument('--info_file_path', type=str, default='data/swissprot_coordinates.json', help='Path to all 3d coordinates file')
-    parser.add_argument('--clustering_path', type=str, default='data/cluster-50/clusterRes_cluster.tsv', help='Path to clustering result file')
+    parser.add_argument('--clustering_path', type=str, default='data/cluster-30/clusterRes_cluster.tsv', help='Path to clustering file')
     args = parser.parse_args()
 
-    #create_data_for_finetuning_task(train_ec_path=args.train_ec_path, test_ec_path=args.test_ec_path, train_3d_path=args.train_3d_path, test_3d_path=args.test_3d_path, info_file_path=args.info_file_path, clustering_path=args.clustering_path)
-   
-
-
-
-
-
-
+    create_data_for_finetuning_task(train_ec_path=args.train_ec_path, test_ec_path=args.test_ec_path, train_3d_path=args.train_3d_path, test_3d_path=args.test_3d_path, info_file_path=args.info_file_path, clustering_path=args.clustering_path)
+    create_data_for_pretraining_task(pretrain_ec_path=args.pretrain_ec_path, test_ec_path=args.test_ec_path, clustering_path=args.clustering_path)
     
-
-
