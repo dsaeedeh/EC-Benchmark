9200031a82d1e02617b317d13f3b2b89ec820332
davoudisaeedeh <saeedeh.davoudi@ucdenver.edu>
2023-07-19 19:47:52 -0600

    modified:   run_mmseqs2.sh

--- run_mmseqs2.sh
+++ run_mmseqs2.sh
@@ -7,7 +7,7 @@
 conda install -c conda-forge -c bioconda mmseqs2
 cd data
 awk 1 *.fasta > all.fasta
-mmseqs easy-cluster all.fasta clusterRes ./ --min-seq-id 0.9 -c 0.8 --cov-mode 1
-mmseqs easy-cluster cluster-90/clusterRes_rep_seq.fasta clusterRes clusters --min-seq-id 0.7 -c 0.8 --cov-mode 1
-mmseqs easy-cluster cluster-70/clusterRes_rep_seq.fasta clusterRes clusters --min-seq-id 0.5 -c 0.8 --cov-mode 1
-mmseqs easy-cluster cluster-50/clusterRes_rep_seq.fasta clusterRes clusters --min-seq-id 0.3 -c 0.8 --cov-mode 1
+mmseqs easy-cluster all.fasta clusterRes cluster-90/ --min-seq-id 0.9 -c 0.8 --cov-mode 1
+mmseqs easy-cluster cluster-90/clusterRes_rep_seq.fasta clusterRes cluster-70/ --min-seq-id 0.7 -c 0.8 --cov-mode 1
+mmseqs easy-cluster cluster-70/clusterRes_rep_seq.fasta clusterRes cluster-50/ --min-seq-id 0.5 -c 0.8 --cov-mode 1
+mmseqs easy-cluster cluster-50/clusterRes_rep_seq.fasta clusterRes cluster-30/ --min-seq-id 0.3 -c 0.8 --cov-mode 1
